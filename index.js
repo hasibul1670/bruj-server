8a0be0e8be84129a443d337607fba54f349de8f3
hasibul1670 <hasibulislam1670.com>
2020-09-30 23:10:14 +0600

    helloasr2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcyjp.mongodb.net/burj-al?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcyjp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
